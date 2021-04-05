c2a99d314a16c60dbed88ba43280c0a3d4f504d1
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-05 08:25:01 +0530

    Add fieldsets to BookInstance Admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,6 +4,7 @@ from .models import Author, Genre, Book, BookInstance
 # Register your models here.
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -19,6 +20,15 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
