169c6b8cf962f49431088bda97d0257812bac0df
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-05 07:48:47 +0530

    Create ModelAdmin classes for models
    - Customize list display in Author and Book
    - Customize list filter in BookInstance

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance
 
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
