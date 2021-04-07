dfa0dc731913cba2f222ecfdc14e3aac024efdae
Rahul Mangal <rahul200826101998@gmail.com>
2021-04-07 17:29:11 +0530

    Add Author functionalities

--- catalog/views.py
+++ catalog/views.py
@@ -11,6 +11,12 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 from catalog.forms import RenewBookForm
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Author
+
 # Create your views here.
 
 def index(request):
@@ -101,3 +107,16 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
